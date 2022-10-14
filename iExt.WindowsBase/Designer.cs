f78e3cc18577f60895349adf2b035a96fda55340
黄钊 <my.world.hz@hotmail.com>
2022-10-14 19:27:43 +0800

    新增扩展：包装 Xaml 设计器的一些属性

---
+++ iExt.WindowsBase/Designer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace System.Windows
+{
+    /// <summary>
+    /// Xaml 设计器
+    /// </summary>
+    public sealed class Designer
+    {
+        /// <summary>
+        /// 表示元素是否在 Xaml 设计器中显示
+        /// </summary>
+        public static bool IsInDesignMode { get; }
+
+        static Designer()
+        {
+            IsInDesignMode = (bool)DependencyPropertyDescriptor.FromProperty(DesignerProperties.IsInDesignModeProperty, typeof(FrameworkElement)).Metadata.DefaultValue;
+        }
+    }
+}
