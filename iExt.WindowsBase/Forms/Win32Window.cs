e46dbcda8c7520d300ac7d9797c66848410f090d
黄钊 <my.world.hz@hotmail.com>
2022-10-14 19:22:54 +0800

    新增扩展：接口 IWin32Window 的 WPF 实现

---
+++ iExt.WindowsBase/Forms/Win32Window.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace System.Windows.Forms
+{
+    /// <summary>
+    /// <see cref="IWin32Window"/> 接口的实现
+    /// </summary>
+    public class Win32Window : IWin32Window
+    {
+        /// <inheritdoc />
+        public IntPtr Handle { get; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dependencyObject"></param>
+        /// <exception cref="Exception"></exception>
+        public Win32Window(DependencyObject dependencyObject)
+        {
+            try
+            {
+                var window = Window.GetWindow(dependencyObject);
+                // ReSharper disable once AssignNullToNotNullAttribute
+                var windowInteropHelper = new WindowInteropHelper(window);
+                Handle = windowInteropHelper.Handle;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("初始化 Win32Window 失败", e);
+            }
+        }
+    }
+}
