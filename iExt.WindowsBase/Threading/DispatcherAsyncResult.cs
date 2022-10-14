f7fd4962a795e23bcba8c8f45461898016921f70
黄钊 <my.world.hz@hotmail.com>
2022-10-14 19:19:30 +0800

    新增扩展功能：ISynchronizeInvoke 的 WPF 实现

---
+++ iExt.WindowsBase/Threading/DispatcherAsyncResult.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace System.Windows.Threading
+{
+    /// <inheritdoc />
+    public class DispatcherAsyncResult : IAsyncResult
+    {
+        private readonly IAsyncResult result;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DispatcherOperation Operation { get; }
+
+        /// <inheritdoc />
+        public bool IsCompleted => result.IsCompleted;
+
+        /// <inheritdoc />
+        public WaitHandle AsyncWaitHandle => result.AsyncWaitHandle;
+
+        /// <inheritdoc />
+        public object AsyncState => result.AsyncState;
+
+        /// <inheritdoc />
+        public bool CompletedSynchronously => result.CompletedSynchronously;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="operation"></param>
+        public DispatcherAsyncResult(DispatcherOperation operation)
+        {
+            Operation = operation;
+            result = operation.Task;
+        }
+    }
+}
---
+++ iExt.WindowsBase/Threading/DispatcherSynchronizeInvoke.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+
+namespace System.Windows.Threading
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DispatcherSynchronizeInvoke : ISynchronizeInvoke
+    {
+        private readonly Dispatcher dispatcher;
+
+        /// <inheritdoc />
+        public bool InvokeRequired => !dispatcher.CheckAccess();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dispatcher"></param>
+        public DispatcherSynchronizeInvoke(Dispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+        }
+
+        /// <inheritdoc />
+        public IAsyncResult BeginInvoke(Delegate method, object[] args)
+        {
+            return new DispatcherAsyncResult(
+                dispatcher.BeginInvoke(method, DispatcherPriority.Normal, args));
+        }
+
+        /// <inheritdoc />
+        public object EndInvoke(IAsyncResult result)
+        {
+            var dispatcherAsyncResult = (DispatcherAsyncResult)result;
+            dispatcherAsyncResult.Operation.Wait();
+            return dispatcherAsyncResult.Operation.Result;
+        }
+
+        /// <inheritdoc />
+        public object Invoke(Delegate method, object[] args)
+        {
+            return dispatcher.Invoke(method, DispatcherPriority.Normal, args);
+        }
+    }
+}
