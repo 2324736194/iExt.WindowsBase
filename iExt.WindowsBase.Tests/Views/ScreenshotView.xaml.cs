bc197525d3bee63108ef5f377be34649f98feefc
黄钊 <my.world.hz@hotmail.com>
2022-10-24 16:42:42 +0800

    新增测试：FrameworkElementExt - 测试元素截图

---
+++ iExt.WindowsBase.Tests/Views/ScreenshotView.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace iExt.WindowsBase.Tests.Views
+{
+    /// <summary>
+    /// ScreenshotView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ScreenshotView : UserControl
+    {
+        private Window displayer;
+
+        public ScreenshotView()
+        {
+            InitializeComponent();
+            Focusable = true;
+        }
+        
+        private void ScreenshotHandler(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                border.ForeachLogicalTree<FrameworkElement>(Handler);
+            }
+        }
+
+        private ForeachFlag Handler(FrameworkElement element)
+        {
+            var result = element.Uid == "Screen shot border";
+            switch (result)
+            {
+                case true:
+                    Debug.WriteLine(nameof(Handler));
+                    var source = element.Screenshot();
+                    var image = new Image();
+                    image.Width = element.ActualWidth;
+                    image.Height = element.ActualHeight;
+                    image.Source = source;
+                    var main = Application.Current.MainWindow;
+                    var events = WeakEventRelay.GetEvents<Window>();
+                    var e = events.Single(p => p.Name == nameof(Window.Activated));
+                    var relay = main.RegisterWeakEvent(e,RegisterDisplayerActivated);
+                    var eHandler = new EventHandler(DisplayerActivatedHandler);
+                    relay.Add(eHandler);
+                    var w = new Window();
+                    displayer = w;
+                    w.Owner = main;
+                    w.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    w.Content = image;
+                    w.SizeToContent = SizeToContent.WidthAndHeight;
+                    w.WindowStyle = WindowStyle.None;
+                    w.ResizeMode = ResizeMode.NoResize;
+                    w.MouseDown += ClickCloseHandler;
+                    w.ShowDialog();
+                    return ForeachFlag.Break;
+                case false:
+                    return ForeachFlag.Normal;
+            }
+        }
+
+        private void RegisterDisplayerActivated(Window owner, WeakEventRelay relay)
+        {
+            owner.Activated += (sender, e) =>
+            {
+                relay.Raise(sender, e);
+            };
+        }
+
+        private void DisplayerActivatedHandler(object sender, EventArgs e)
+        {
+            Debug.WriteLine(nameof(DisplayerActivatedHandler));
+            if (null == displayer) return;
+            displayer.Activate();
+        }
+
+        private void ClickCloseHandler(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Window window)
+            {
+                displayer = null;
+                window.Close();
+            }
+        }
+    }
+}
---
+++ iExt.WindowsBase/FrameworkElementExt.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Linq;
+
+namespace System.Windows
+{
+    /// <summary>
+    /// <see cref="FrameworkElement"/> 扩展
+    /// </summary>
+    public static class FrameworkElementExt
+    {
+        /// <summary>
+        /// 对元素进行截图
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="dpi"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static RenderTargetBitmap Screenshot(this FrameworkElement element,
+            Dpi dpi, PixelFormat format)
+        {
+            if (null == element)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+            if (double.IsNaN(element.ActualHeight) || 
+                double.IsNaN(element.ActualWidth) || 
+                element.ActualWidth < 1 ||
+                element.ActualHeight < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(element));
+            }
+            var width = (int)element.ActualWidth;
+            var height = (int)element.ActualHeight;
+            var source = new RenderTargetBitmap(width, height, dpi.X, dpi.Y, format);
+            source.Render(element);
+            return source;
+        }
+
+        /// <summary>
+        /// <para>对元素进行截图</para>
+        /// <para>注意：</para>
+        /// <para>当前元素的高宽不允许为 0</para>
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static RenderTargetBitmap Screenshot(this FrameworkElement element)
+        {
+            var dpiScale = Dpi.GetDpiFromVisual(element);
+            var format = PixelFormats.Default;
+            return Screenshot(element, dpiScale, format);
+        }
+    }
+}
