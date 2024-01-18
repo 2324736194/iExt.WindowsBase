043c1e0ea3aba9473ea01a373c6c065f9b8754c0
黄钊 <my.world.hz@hotmail.com>
2024-01-18 10:36:59 +0800

    新增扩展：ScreenshotHandler

---
+++ iExt.WindowsBase/DpiScale.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Windows.Media;
+
+namespace System.Windows
+{
+    /// <summary>
+    /// 将从中 DPI 信息存储 <see cref="Visual"/> 或 <see cref="UIElement"/> 呈现。
+    /// </summary>
+    public readonly struct DpiScale
+    {
+        private readonly double _pixelsPerInchX;
+        private readonly double _pixelsPerInchY;
+
+        /// <summary>
+        /// 获取 DPI X 轴
+        /// </summary>
+        public double PixelsPerInchX =>_pixelsPerInchX;
+
+        /// <summary>
+        /// 获取沿 Y 轴的 DPI
+        /// </summary>
+        public double PixelsPerInchY => _pixelsPerInchY;
+
+        /// <summary>
+        /// 初始化 <see cref="DpiScale"/> 结构的新实例
+        /// </summary>
+        /// <param name="visual">视觉对象</param>
+        public DpiScale(Visual visual)
+        {
+            _pixelsPerInchX = 96.0;
+            _pixelsPerInchY = 96.0;
+
+            var source = PresentationSource.FromVisual(visual);
+            if (source?.CompositionTarget != null)
+            {
+                _pixelsPerInchX *= source.CompositionTarget.TransformToDevice.M11;
+                _pixelsPerInchY *= source.CompositionTarget.TransformToDevice.M22;
+            }
+        }
+    }   
+}
--- iExt.WindowsBase/ScreenshotHandler.cs
+++ iExt.WindowsBase/ScreenshotHandler.cs
@@ -1,37 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
+﻿using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Xml.Linq;
 
 namespace System.Windows
 {
     /// <summary>
-    /// <see cref="FrameworkElement"/> 扩展
+    /// 截图处理
     /// </summary>
-    public static class FrameworkElementExt
-    {
+    public sealed class ScreenshotHandler
+    {       
         /// <summary>
-        /// 对元素进行截图
+        /// 截图
         /// </summary>
         /// <param name="element"></param>
-        /// <param name="dpi"></param>
+        /// <param name="dpiScale"></param>
         /// <param name="format"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static RenderTargetBitmap Screenshot(this FrameworkElement element,
-            Dpi dpi, PixelFormat format)
-        {
+        public ImageSource Handle(FrameworkElement element, DpiScale dpiScale, PixelFormat format)
+        {           
             if (null == element)
-            {
+            {   
                 throw new ArgumentNullException(nameof(element));
             }
-            if (double.IsNaN(element.ActualHeight) || 
-                double.IsNaN(element.ActualWidth) || 
+            if (double.IsNaN(element.ActualHeight) ||
+                double.IsNaN(element.ActualWidth) ||
                 element.ActualWidth < 1 ||
                 element.ActualHeight < 1)
             {
@@ -39,23 +32,21 @@ namespace System.Windows
             }
             var width = (int)element.ActualWidth;
             var height = (int)element.ActualHeight;
-            var source = new RenderTargetBitmap(width, height, dpi.X, dpi.Y, format);
+            var source = new RenderTargetBitmap(width, height, dpiScale.PixelsPerInchX, dpiScale.PixelsPerInchY, format);
             source.Render(element);
             return source;
         }
 
         /// <summary>
-        /// <para>对元素进行截图</para>
-        /// <para>注意：</para>
-        /// <para>当前元素的高宽不允许为 0</para>
+        /// 截图
         /// </summary>
         /// <param name="element"></param>
         /// <returns></returns>
-        public static RenderTargetBitmap Screenshot(this FrameworkElement element)
+        public ImageSource Handle(FrameworkElement element)
         {
-            var dpiScale = Dpi.GetDpiFromVisual(element);
+            var dpiScale = new DpiScale(element);
             var format = PixelFormats.Default;
-            return Screenshot(element, dpiScale, format);
+            return Handle(element, dpiScale, format);
         }
     }
 }
